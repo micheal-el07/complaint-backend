abac8216cd110e3913554ce89b48172ac71292b1
Mikhail <mikhailkamal00@gmail.com>
2025-02-16 00:19:03 +0800

    customerror file

--- src/utils/customError.ts
+++ src/utils/customError.ts
@@ -1,13 +1,13 @@
 export class ClassificationError extends Error {
-    constructor(message = "Failed to classify complaint description") {
-        super(message);
-        this.name = "ClassificationError";
-    }
+  constructor(message = "Failed to classify complaint description") {
+    super(message);
+    this.name = "ClassificationError";
+  }
 }
 
 export class DatabaseError extends Error {
-    constructor(message = "Database operation failed") {
-        super(message);
-        this.name = "DatabaseError"
-    }
+  constructor(message = "Database operation failed") {
+    super(message);
+    this.name = "DatabaseError";
+  }
 }
